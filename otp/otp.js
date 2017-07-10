15c62bc8f1a734b3065e122ee14fa28711c47de6
Pablo Di Noto <pdinoto@gmail.com>
2017-07-10 19:27:27 +0000

    First attempt for TOTP webtask

---
+++ otp/otp.js
@@ -0,0 +1,94 @@
+
+module.exports = function(context, cb) {
+
+  var OTP = require('otp.js');
+  var HOTP = OTP.hotp;
+  var GA = OTP.googleAuthenticator;
+  var response = '';
+  var secret = '';
+
+  if (typeof context.secrets.GAsecret !== 'undefined') {
+    secret = context.secrets.GAsecret
+    console.log("Got secret from webtask.")
+  } else if (typeof context.query['GAsecret'] !== 'undefined') {
+    secret = context.secrets.GAsecret
+    console.log("Got secret from webtask.")
+  } else {
+    secret = 'caramba!';
+  }
+
+  try {
+    // generate otp for base 32 encoded user secret
+    var code = GA.gen(GA.encode(secret));
+    var today = new Date();
+    cb(null, { code: code, date: today.toISOString() });
+    console.log(code); // print otp result => 6-digit number
+  }
+  catch(ex) {
+    console.error(ex); // print error occurred during OTP generation process
+  }
+
+}
+
+
+
+exports.express = function (options, cb) {
+  options.nodejsCompiler(options.script, function (error, func) {
+    if (error) return cb(error);
+    try {
+      func = fromConnect(func); // this is where adaptation happens
+    }
+    catch (e) {
+      return cb(e);
+    }
+    return cb(null, func);
+  });
+};
+
+
+
+// Code Verification
+// try {
+//   // verify otp 'XXXXXX' for base 32 encoded user secret
+//   var result = GA.verify(code, GA.encode('caramba!'));
+//   console.log(result); // print result => {delta:#}
+// }
+// catch(ex) {
+//   console.error(ex); // print error occurred during OTP verification process
+// }
+
+// Simple HOTP test
+// try {
+//   // generate otp for key '12345678901234567890' in string format
+//   var code = HOTP.gen({string:'12345678901234567890'});
+//   console.log(code); // print otp result => 755224
+// }
+// catch(ex) {
+//   console.error(ex); // print error occurred during OTP generation process
+// }
+
+// try {
+//   // verify otp '755224' for key '12345678901234567890' in string format
+//   var result = HOTP.verify('755224', {string:'12345678901234567890'});
+//   console.log(result); // print result => {delta:{int:0}}
+// }
+// catch(ex) {
+//   console.error(ex); // print error occurred during OTP verification process
+// }
+
+// Google Authenticator
+// get GoogleAuthenticator object
+// hardcode secret for console testing
+
+
+// // QRCode Generation
+// try {
+//   // generate base32 secret
+//   var secret = GA.encode('caramba!') || GA.secret();
+//   // get QRCode in SVG format
+//   var qrCode = GA.qrCode('Auth0 webtask test', 'webtask.io+otp.js', secret);
+//   console.log(qrCode); // print svg => <svg xmlns="http://www.w3.org/2000/svg" width="215" height="215" viewBox="0 0 43 43">...</svg>
+// }
+// catch(ex) {
+//   console.error(ex); // print error occurred during QRCode generation
+// }
---
+++ otp/readme.md
@@ -0,0 +1,4 @@
+
+### OTP webtask
+
+An attempt to have a webtask that given a series of a secret will provide a proper [TOPT](https://en.wikipedia.org/wiki/Time-based_One-time_Password_Algorithm) code to be used as 2FA
